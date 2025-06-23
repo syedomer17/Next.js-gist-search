d4e601f52dbdacacf91d03e8802529e054406c38
syedomer17 <syedomerali2006@gmail.com>
2025-06-23 21:10:25 +0530

    Updated by Omer on 2025-06-23 at 09:10PM

--- gits/src/app/api/token-test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
-
-export async function GET(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  console.log("🔐 Token from getToken():", token);
-  return NextResponse.json({ token });
-}
